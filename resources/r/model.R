1ea1847bf12b1bbf310834b1393144ba12e3dab5
KathrynLittleboy <Kathryn.littleboy@ons.gov.uk>
2017-04-04 14:41:59 +0100

    add region function
    assign variable names

--- resources/r/model.R
+++ resources/r/model.R
@@ -1,5 +1,7 @@
 #install.packages("data.table")
+#install.packages("dplyr")
 library(data.table)
+library(dplyr)
 
 #CCG Data
 CCG_prevalence <- read.csv("data/Estimated_Prevalence_of_CMDs_2014-2015.csv")
@@ -12,5 +14,19 @@ aggregate_prevalence_to_England <- function(prevalence_data) {
   return(England_prevalence)
 }
 
+#Function to aggregate to Region
+aggregate_prevalence_to_region <- function(prevalence_data) {
+  regional_level_prevalence <- prevalence_data %>%
+    group_by(Parent.Code, Parent.Name) %>%
+    summarise(Count = sum(Count),
+              Population = sum(Denominator)) %>%
+    mutate(prevalence=(Count/Population)*100)
+  return(regional_level_prevalence)
+}
+
 #Run function, specifying dataset to use
-aggregate_prevalence_to_England(CCG_prevalence)
+england_prevalence <- aggregate_prevalence_to_England(CCG_prevalence)
+region_prevalence <- aggregate_prevalence_to_region(CCG_prevalence)
+
+
+
