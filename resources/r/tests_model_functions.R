8ff549b61acd0717c37f5fd0c64af4d74cce96e0
KathrynLittleboy <Kathryn.littleboy@ons.gov.uk>
2017-04-25 15:48:07 +0100

    Explain why we needed a function to manipulate the regions to match the shapefil

--- resources/r/tests_model_functions.R
+++ resources/r/tests_model_functions.R
@@ -12,6 +12,8 @@ test_that("Test that the number of  NHS regions is 14",
                       expect_equal(number_of_rows, 14)})
 
 #Test for function to mainpulate regions to match shapefile
+#Our dataset had 14 regions but the shapefile combined two of these (greater manchester and lancashire) so only had 13 regions
+#We created a function to combine these two regions in our dataset so we were left with 13 regions, matching the shapefile
 test_that("Test that the number of  NHS regions is 13",
           {CCG_prevalence <- read.csv("data/Estimated_Prevalence_of_CMDs_2014-2015.csv")
           thirteen_level_NHS_regional_prevalence <- manipulate_regions_for_shapefile(region_prevalence)
