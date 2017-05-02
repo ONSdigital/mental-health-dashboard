cbb751281e3ca8447d7aba77aba4adb05436b2d6
Seb Bailey <sebastian@mastodonc.com>
2017-05-02 16:58:38 +0100

    Fixed typo

--- src/r/app.R
+++ src/r/app.R
@@ -3,7 +3,7 @@ library(shiny)
 source("src/r/model.r")
 
 ui <- shinyUI(fluidPage(
-  fluidRow(h1("This the page layout a user would see after clicking on a map region")),
+  fluidRow(h1("This is the page layout a user would see after clicking on a map region")),
   fluidRow(column(width = 4, style = div(style = "height:100px;"), plotOutput("map", width = 1000)),
     column(width = 5, offset = 1, div(style = "height:100px;"), plotOutput("chart", width = 1000)),
   fluidRow(column(width = 12, style = "background-color:white;", div(style = "height:200px;"))), #creates white space
