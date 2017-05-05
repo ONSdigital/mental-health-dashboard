3821901968c1bd4d78d43feb7f1ac4ec229e6117
Seb Bailey <sebastian@mastodonc.com>
2017-05-05 10:49:36 +0100

    Fixed display both app bug

--- src/r/app.R
+++ src/r/app.R
@@ -1,6 +1,6 @@
 #install.packages("shiny")
 library(shiny)
-source("src/r/model.r")
+source("src/r/model.R")
 
 ui <- shinyUI(fluidPage(
   fluidRow(h1("This is the page layout a user would see after clicking on a map region")),
@@ -14,10 +14,10 @@ server <- function(input, output) {
   output$map <- renderPlot(height = 600, {
     choropleth_map_prevalence_by_NHS_Region <- create_choropleth_map_by_prevalence(model_outputs[[1]])
     })
-  # currently second output doesn't work
-  #output$chart <- renderPlot(height = 1000 {
-  #  create_barchart_of_prevalence_by_region(model_outputs[[2]], model_outputs[[3]], region)
-  #})
+#  currently second output doesn't work
+  output$chart <- renderPlot(height = 1000, {
+   create_barchart_of_prevalence_by_region(model_outputs[[2]], model_outputs[[3]], region)
+  })
   }
 
 shinyApp(ui = ui, server = server)
