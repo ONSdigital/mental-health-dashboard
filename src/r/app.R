687738600dc75d2d103a4c97e6a9406103d27937
DanielleCornish <danielle.cornish@ons.gov.uk>
2017-05-05 12:28:56 +0100

    reindent lines

--- src/r/app.R
+++ src/r/app.R
@@ -5,21 +5,20 @@ source("src/r/model.R")
 ui <- shinyUI(fluidPage(
   fluidRow(h1("Prevalence of Common Mental Health Disorders in England, by NHS Region, 2014/15")),
   fluidRow(column(width = 6, plotOutput("map", height = 800)),
-    column(width = 6, plotOutput("chart", height = 800))),
-fluidRow(h2(textOutput("narrative"))),
-fluidRow(h3("For more information on this dataset click", a("here", href= "https://github.com/ONSdigital/mental-health-dashboard", target="_blank"), "."))))
-
+           column(width = 6, plotOutput("chart", height = 800))),
+  fluidRow(h2(textOutput("narrative"))),
+  fluidRow(h3("For more information on this dataset click", a("here", href= "https://github.com/ONSdigital/mental-health-dashboard", target="_blank"), "."))))
 
 
 server <- function(input, output) {
   output$map <- renderPlot( {
     choropleth_map_prevalence_by_NHS_Region <- create_choropleth_map_by_prevalence(model_outputs[[1]])
-    })
+  })
   output$chart <- renderPlot({
-   create_barchart_of_prevalence_by_region(model_outputs[[2]], model_outputs[[3]], region)
+    create_barchart_of_prevalence_by_region(model_outputs[[2]], model_outputs[[3]], region)
   })
   output$narrative <- renderText({narrative_by_region <- create_narrative(narrative, region)})
-  }
+}
 
 shinyApp(ui = ui, server = server)
 
