cb45435f010c630d94534e7fefe0a488338237c9
Seb Bailey <sebastian@mastodonc.com>
2017-05-09 14:57:09 +0100

    Add dropdown box

--- app.R
+++ app.R
@@ -14,28 +14,27 @@ ui <- shinyUI(fluidPage(
   fluidRow(column(1),
            column(10, h1("Prevalence of Common Mental Health Disorders among people aged 16 to 74,\n in England, by NHS Region, 2014/15")),
             column(1)),
-  fluidRow(h2("")),
+  fluidRow(sidebarPanel(
+    selectInput('region', 'Please select region', model_outputs[[2]]$Parent.Name))),
   fluidRow(column(5, plotOutput("map", height = "600")),
            column(7, plotOutput("chart", height = "500", width = "900"))),
   fluidRow(column(1),
            column(10,(h2(textOutput("narrative")))),
           column(1)),
   fluidRow(column(1),
-           column(10, h3("For more information on this dataset click", 
+           column(10, h3("For more information on this dataset click",
                          a("here", href= "https://github.com/ONSdigital/mental-health-dashboard/blob/master/src/r/data/Metadata.md", target="_blank"), ".")),
            column(1)))
 )
-
 
 server <- function(input, output) {
   output$map <- renderPlot( {
     create_choropleth_map_by_prevalence(model_outputs[[1]])
   })
   output$chart <- renderPlot({
-    create_barchart_of_prevalence_by_region(model_outputs[[2]], model_outputs[[3]], region)
+    create_barchart_of_prevalence_by_region(model_outputs[[2]], model_outputs[[3]], input$region)
   })
-  output$narrative <- renderText({narrative_by_region <- create_narrative(narrative, region)})
+  output$narrative <- renderText({narrative_by_region <- create_narrative(narrative, input$region)})
 }
 
 shinyApp(ui = ui, server = server)
-
