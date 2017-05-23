813b9d6a7f1f934229a88a435cf84fd17dde69ba
KathrynLittleboy <Kathryn.littleboy@ons.gov.uk>
2017-05-23 13:03:25 +0100

    display 3 datasets in app
    narrative is still wrong

--- app.R
+++ app.R
@@ -23,9 +23,9 @@ ui <- shinyUI(
                   fluidRow(sidebarPanel( 
                     tags$style(type='text/css', ".selectize-input { font-size: 20px;} .selectize-dropdown { font-size: 20px;}"),
                   selectInput('region', label = h3('Please select an NHS region'), model_outputs[[2]]$Parent.Name))),
-                  fluidRow(plotOutput("chart")),
-                  fluidRow(plotOutput("map")),
-                  fluidRow(h2(textOutput("narrative"))),
+                  fluidRow(plotOutput("chart1")),
+                  fluidRow(plotOutput("map1")),
+                  fluidRow(h2(textOutput("narrative1"))),
                   fluidRow(h3("For more information on this dataset click",
                      a("here", href= "https://github.com/ONSdigital/mental-health-dashboard/blob/master/src/r/data/Metadata.md", target="_blank"), "."))
         ),
@@ -34,7 +34,12 @@ ui <- shinyUI(
                  fluidRow(h1(" Percentage of patients on GP practice register, aged 18+, recorded as having depression,\n in England, by NHS Region, 2014/15")),
                  fluidRow(sidebarPanel( 
                    tags$style(type='text/css', ".selectize-input { font-size: 20px;} .selectize-dropdown { font-size: 20px;}"),
-                   selectInput('region', label = h3('Please select an NHS region'), model_outputs[[2]]$Parent.Name)))
+                   selectInput('region', label = h3('Please select an NHS region'), model_outputs[[2]]$Parent.Name))),
+                 fluidRow(plotOutput("chart2")),
+                 fluidRow(plotOutput("map2")),
+                 fluidRow(h2(textOutput("narrative2"))),
+                 fluidRow(h3("For more information on this dataset click",
+                             a("here", href= "https://github.com/ONSdigital/mental-health-dashboard/blob/master/src/r/data/Metadata.md", target="_blank"), "."))
                  
         ),
         
@@ -42,7 +47,12 @@ ui <- shinyUI(
                  fluidRow(h1("Percentage of newly diagnosed patients with depression, aged 18+, who had a review 10-56 days after diagnosis,\n in England, by NHS Region, 2014/15")),
                  fluidRow(sidebarPanel( 
                    tags$style(type='text/css', ".selectize-input { font-size: 20px;} .selectize-dropdown { font-size: 20px;}"),
-                   selectInput('region', label = h3('Please select an NHS region'), model_outputs[[2]]$Parent.Name)))
+                   selectInput('region', label = h3('Please select an NHS region'), model_outputs[[2]]$Parent.Name))),
+                 fluidRow(plotOutput("chart3")),
+                 fluidRow(plotOutput("map3")),
+                 fluidRow(h2(textOutput("narrative3"))),
+                 fluidRow(h3("For more information on this dataset click",
+                             a("here", href= "https://github.com/ONSdigital/mental-health-dashboard/blob/master/src/r/data/Metadata.md", target="_blank"), "."))
                 
            )
         
@@ -60,13 +70,29 @@ ui <- shinyUI(
   
 
 server <- function(input, output) {
-  output$map <- renderPlot( {
-    create_choropleth_map_by_prevalence(model_outputs[[1]], input$region)
+  output$map1 <- renderPlot( {
+    create_choropleth_map_by_prevalence(model_outputs1[[1]], input$region)
   })
-  output$chart <- renderPlot({
-    create_barchart_of_prevalence_by_region(model_outputs[[2]], model_outputs[[3]], input$region)
+  output$chart1 <- renderPlot({
+    create_barchart_of_prevalence_by_region(model_outputs1[[2]], model_outputs1[[3]], input$region)
   })
-  output$narrative <- renderText({create_narrative(model_outputs, input$region)})
+  output$narrative1 <- renderText({create_narrative(model_outputs1, input$region)})
+  
+  output$map2 <- renderPlot( {
+    create_choropleth_map_by_prevalence(model_outputs2[[1]], input$region)
+  })
+  output$chart2 <- renderPlot({
+    create_barchart_of_prevalence_by_region(model_outputs2[[2]], model_outputs2[[3]], input$region)
+  })
+  output$narrative2 <- renderText({create_narrative(model_outputs2, input$region)})
+  
+  output$map3 <- renderPlot( {
+    create_choropleth_map_by_prevalence(model_outputs3[[1]], input$region)
+  })
+  output$chart3 <- renderPlot({
+    create_barchart_of_prevalence_by_region(model_outputs3[[2]], model_outputs3[[3]], input$region)
+  })
+  output$narrative3 <- renderText({create_narrative(model_outputs3, input$region)})
 }
 
 shinyApp(ui = ui, server = server)
