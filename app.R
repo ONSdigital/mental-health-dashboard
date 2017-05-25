858a169696dab91204000c71ff9c691254c6adb4
DanielleCornish <danielle.cornish@ons.gov.uk>
2017-05-25 08:37:00 +0100

    change dropdown order to alphabetical

--- app.R
+++ app.R
@@ -16,7 +16,7 @@ format_tab <- function(title, header, region_no, map_no, chart_no, narrative_no,
            fluidRow(column(1), column( 10,h1(header)),column(1)), 
            fluidRow(column(1), column( 10,sidebarPanel( 
              tags$style(type='text/css', ".selectize-input { font-size: 20px;} .selectize-dropdown { font-size: 20px;}"),
-             selectInput(region_no, label = h3('Please select an NHS region'), model_outputs1[[2]]$Parent.Name)))),
+             selectInput(region_no, label = h3('Please select an NHS region'), model_outputs1[[2]]$Parent.Name[order(model_outputs1[[2]]$Parent.Name)])))),
            fluidRow(column(5, plotOutput(map_no,height = "700")),
                     (column(7, plotOutput(chart_no, height = "500", width = "1000")))),
            fluidRow(column(1), column(10, h2(textOutput(narrative_no)))), column(1),
@@ -31,7 +31,7 @@ comparison_tab <- function (title, header, region_no, chart1_no, chart2_no, char
            fluidRow(column(1), column( 10,h1(header)),column(1)), 
            fluidRow(column(1), column( 10,sidebarPanel( 
              tags$style(type='text/css', ".selectize-input { font-size: 20px;} .selectize-dropdown { font-size: 20px;}"),
-             selectInput(region_no, label = h3('Please select an NHS region'), model_outputs1[[2]]$Parent.Name)))),
+             selectInput(region_no, label = h3('Please select an NHS region'), model_outputs1[[2]]$Parent.Name[order(model_outputs1[[2]]$Parent.Name)])))),
            fluidRow (column(6, plotOutput(chart1_no, width = "900")),
                      (column(6, plotOutput(chart2_no, width = "900")))),
            fluidRow(column(1)),
