ef3bf90acdd11f536077fa9d1d8dc42ef2241d6c
KathrynLittleboy <Kathryn.littleboy@ons.gov.uk>
2017-05-25 15:58:35 +0100

    Line chart filtered by single region

--- src/r/Create_line_chart.R
+++ src/r/Create_line_chart.R
@@ -11,23 +11,21 @@ library(dplyr)
 #Suicides Time series Data
 Suicides_time_series_raw <- read.csv("src/r/data/REgion_Suicide_Time_Series.csv", check.names = F)
 
-Suicides_time_series_reshaped <- melt(Suicides_time_series_raw, id.vars="Region.name", value.name="Rate", variable.name="Year")
 
 #Function for tie series
 create_suicide_time_series <- function(Suicide_data, nhs_region) {
 #reformat dataframe for line chart 
 Suicides_time_series_reshaped <- melt(Suicide_data, id.vars="Region.name", value.name="Rate", variable.name="Year")
+#Filter data by single region
+Specific_region <- filter(Suicides_time_series_reshaped, Region.name == region)
 #Plot line chart
-suicides_chart <- ggplot(data = Suicides_time_series_reshaped, aes(x=Year, y=Rate, group = Region.name, colour =  Region.name)) +
-  geom_line() +
-  scale_fill_manual(values = Region.name) +
-  geom_line(stat = "identity", size=1, aes(colour=Region.name==nhs_region))
+suicides_chart <- ggplot(data = Specific_region, aes(x=Year, y=Rate, group = Region.name)) +
+  geom_line() 
+  #scale_fill_manual(values = Region.name) +
+  #geom_line(stat = "identity", size=1, aes(colour=Region.name==nhs_region))
 }
 
 region = "Wessex"
 
 timeseries <- create_suicide_time_series(Suicides_time_series_raw, region)
 plot(timeseries)
-
-
-  
