c510fe9d43ca6f9223f77bab70241e2166643041
KathrynLittleboy <Kathryn.littleboy@ons.gov.uk>
2017-05-26 10:46:22 +0100

    move 2013/14 data to start of intro page

--- app.R
+++ app.R
@@ -75,11 +75,12 @@ ui <- shinyUI(
                home_tab("Overview", 
                         "This dashboard presents mental health data taken from Public Health England (PHE) Fingertips tool and the Office for National Statistics (ONS). 
 Read on to learn about England overall, or click the tabs above to explore regional breakdowns.",
+                        "In 2013/14 the spending on mental health in England was £145.80 per 1,000 population.",
                         "In 2014/15 the overall prevalence of common mental disorders in England was 15.6%.",
                         "In 2014/15 the percentage of patients on GP practice register recorded as having depression in England was 7.3%.",
                         "In 2014/15 the percentage of newly diagnosed patients with depression who had a review 10-56 days after diagnosis in England was 63.8%.",
-                        "In 2015 the age-standardised suicide-rate in England was 10.1 per 100,000 population.",
-                        "In 2013/14 the spending on mental health in England was £145.80 per 1,000 population."),
+                        "In 2015 the age-standardised suicide-rate in England was 10.1 per 100,000 population."
+                        ),
                
                
                format_tab("Spending on Mental Health 2013/14", 
